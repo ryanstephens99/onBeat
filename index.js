a2e03c03ccd8edcee49092da73dba6863ba46db3
Ryan Stephens <ryanstephens@MacBook-Pro.local>
2020-03-12 13:59:13 -0500

    changed name to OnBeat

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('Routinely', () => App);
+AppRegistry.registerComponent('OnBeat', () => App);
 
 if (Platform.OS === 'web') {
   const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('Routinely', { rootTag });
+  AppRegistry.runApplication('OnBeat', { rootTag });
 }
