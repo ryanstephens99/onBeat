f527c5a105bcce714d74db1a47a5b3124b6dc3c1
Ryan Stephens <ryanstephens@MacBook-Pro.local>
2020-04-19 21:45:44 -0500

    re-added files

--- onBeat_react_native/src/screens/AccountScreen.js
+++ onBeat_react_native/src/screens/AccountScreen.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Button } from 'react-native-elements';
 import styles from './styles';
+import SpotifyAPI from './../spotify';
 
 class AccountScreen extends Component {
 
-  
+  constructor(props) {
+    super(props)
+    this.spotify = new SpotifyAPI()
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Button 
             title= "Connect to Spotify"
-            onPress={}
+            onPress={async () => await this.spotify.login()}
         />
         <Button 
             title= "View Plans"
